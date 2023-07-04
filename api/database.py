7c8698fc8a20a27d9329a526a30982a84f8db2c7
Carlos Perilla <carlosperillaprogramacion@gmail.com>
2023-07-04 07:37:18 -0300

    Estableciendo modelos

--- api/database.py
+++ api/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = f"sqlite:///./transferencias_autos.db"
 
+# check_same_thread es necesario solo condición necesaria para squlite.
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
